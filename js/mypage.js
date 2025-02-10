8e507d4a8df802a2881b07cbc4ef88c11c2cffe0
parkjieun <p_n49@naver.com>
2025-02-10 17:58:40 +0900

    250210 반응형 배너 확인

--- js/mypage.js
+++ js/mypage.js
@@ -1,12 +1,13 @@
 // 하단 광고 배너 슬라이드 부분
-let swiper = new Swiper(".swiper-container", {
+const swiper = new Swiper(".swiper", {
+  direction: "horizontal",
   loop: true,
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
+  },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
   },
 });
